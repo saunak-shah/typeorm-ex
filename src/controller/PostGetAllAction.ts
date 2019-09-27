a69104fecaf34a1ab54ca28ec6f0da2947605e4e
saunak-shah <saunakpshah@gmail.com>
2019-09-27 11:22:06 +0530

    Initial commit

---
+++ src/controller/PostGetAllAction.ts
@@ -0,0 +1,19 @@
+import {Request, Response} from "express";
+import {getManager} from "typeorm";
+import {Post} from "../entity/Post";
+
+/**
+ * Loads all posts from the database.
+ */
+export async function postGetAllAction(request: Request, response: Response) {
+
+    response.send({success:1, message:"Api successfull."})
+    // get a post repository to perform operations with post
+    // const postRepository = getManager().getRepository(Post);
+
+    // // load a post by a given post id
+    // const posts = await postRepository.find();
+
+    // // return loaded posts
+    // response.send(posts);
+}
